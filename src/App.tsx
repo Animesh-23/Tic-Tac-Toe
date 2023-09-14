5ce4e4cf4d51020176d63e50201a63830eebf5b7
Animesh <animeshyesugade2002@gmail.com>
2023-09-14 10:36:46 +0530

    added logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
+import { useState } from "react";
 import Cell from "./components/Cell";
 
 const App = () => {
+  const [turn, setTurn] = useState("X");
+  const [winner, setWinner] = useState("");
+  const playerX: Set<number> = new Set<number>();
+  const playerO: Set<number> = new Set<number>();
   return (
-    <div className="flex justify-center  h-screen bg-slate-400">
-      <div className="">
-        <h1 className=" font-semibold text-white text-8xl font-head text-center my-20">
+    <div className="flex  items-center h-screen bg-slate-400 ">
+      <div className=" ml-10">
+        <span className=" text-white font-medium text-5xl px-3">
+          Player
+          <span className=" text-black text-semibold mx-3 inline-block w-10 text-center">
+            {turn}
+          </span>
+          Move
+        </span>
+      </div>
+      <div className=" mx-40">
+        <h1 className=" font-semibold text-white text-8xl font-head text-center my-16">
           Tic Tac Toe
         </h1>
-        <div className=" shadow-xl grid grid-cols-3 grid-rows-3 gap-4">
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
+        <div className=" grid grid-cols-3 grid-rows-3 gap-4 ">
+          {Array.from({ length: 9 }).map((_, i) => (
+            <Cell
+              key={i}
+              turn={turn}
+              setTurn={setTurn}
+              id={i + 1}
+              playerX={playerX}
+              playerO={playerO}
+              setWinner={setWinner}
+            />
+          ))}
         </div>
+      </div>
+      <div className=" h-36 w-36">
+        {winner !== "" && <span>Winner is {winner}</span>}
       </div>
     </div>
   );
--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,84 @@
-const Cell = () => {
+import { useState } from "react";
+const Cell = (props: {
+  turn: string;
+  setTurn: React.Dispatch<React.SetStateAction<string>>;
+  id: number;
+  playerO: Set<number>;
+  playerX: Set<number>;
+  setWinner: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const [val, setVal] = useState("");
+  function add() {
+    if (props.turn === "X") {
+      props.playerX.add(props.id);
+      if (
+        (props.playerX.has(1) &&
+          props.playerX.has(2) &&
+          props.playerX.has(3)) ||
+        (props.playerX.has(4) &&
+          props.playerX.has(5) &&
+          props.playerX.has(6)) ||
+        (props.playerX.has(7) &&
+          props.playerX.has(8) &&
+          props.playerX.has(9)) ||
+        (props.playerX.has(1) &&
+          props.playerX.has(4) &&
+          props.playerX.has(7)) ||
+        (props.playerX.has(2) &&
+          props.playerX.has(5) &&
+          props.playerX.has(8)) ||
+        (props.playerX.has(3) &&
+          props.playerX.has(6) &&
+          props.playerX.has(9)) ||
+        (props.playerX.has(1) &&
+          props.playerX.has(5) &&
+          props.playerX.has(9)) ||
+        (props.playerX.has(3) && props.playerX.has(5) && props.playerX.has(7))
+      ) {
+        props.setWinner("X");
+      }
+    } else {
+      props.playerO.add(props.id);
+      if (
+        (props.playerO.has(1) &&
+          props.playerO.has(2) &&
+          props.playerO.has(3)) ||
+        (props.playerO.has(4) &&
+          props.playerO.has(5) &&
+          props.playerO.has(6)) ||
+        (props.playerO.has(7) &&
+          props.playerO.has(8) &&
+          props.playerO.has(9)) ||
+        (props.playerO.has(1) &&
+          props.playerO.has(4) &&
+          props.playerO.has(7)) ||
+        (props.playerO.has(2) &&
+          props.playerO.has(5) &&
+          props.playerO.has(8)) ||
+        (props.playerO.has(3) &&
+          props.playerO.has(6) &&
+          props.playerO.has(9)) ||
+        (props.playerO.has(1) &&
+          props.playerO.has(5) &&
+          props.playerO.has(9)) ||
+        (props.playerO.has(3) && props.playerO.has(5) && props.playerO.has(7))
+      ) {
+        props.setWinner("X");
+      }
+    }
+  }
   return (
     <>
-      <button className=" bg-white h-32 w-32 rounded-lg">1</button>
+      <button
+        className=" bg-white h-32 w-32 rounded-3xl shadow-2xl text-6xl font-semibold"
+        onClick={() => {
+          add();
+          props.setTurn((prevTurn) => (prevTurn === "X" ? "O" : "X"));
+          setVal(() => props.turn);
+        }}
+      >
+        {val}
+      </button>
     </>
   );
 };
